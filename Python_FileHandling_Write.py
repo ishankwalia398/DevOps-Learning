b3042401bf2d68493149a8005a51302f12de20b8
ishankwalia398 <ishank.walia398@gmail.com>
2026-03-28 20:59:52 +0530

    Update Python_FileHandling_Write.py

--- Python_FileHandling_Write.py
+++ Python_FileHandling_Write.py
@@ -1,4 +1,4 @@
-f = open('data.txt', 'r+')  # Open a file in write mode
+f = open('Python_data.txt', 'r+')  # Open a file in write mode
 
 data = f.read()  # Read the content of the file
 print(data)
